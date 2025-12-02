28c29d6a402c67371549c525f69578e0ae9b74b9
netizen368 <rlawlsdud20302@gmail.com>
2025-12-02 16:37:06 +0900

    자바 2일차 : 중첩 반복문 연습 예제

---
+++ java_kjn/src/day02/EX08_NestedForTest.java
@@ -0,0 +1,40 @@
+package day02;
+
+public class EX08_NestedForTest {
+
+	public static void main(String[] args) {
+
+		/* num가 소수이면 num을 출력하고 아니면 출력하지 않은 코드를 작성하세요.
+		 * 소수는 약수가 2개
+		 * 약수는 나누어 떨어지는 수
+		 * 8의 약수 : 1, 2, 4, 8
+		 */
+		
+		int num = 2, count = 0;
+		
+		// 예전 예제 : 2는 소수, 지금 예제 : 2
+		// 예전 예제 : 4는 소수 아님, 지금 예제:
+		for(int i = 1; i <= num; i++) {
+			if(num % i == 0) {count = count + 1;}
+		}
+		if(count == 2) {System.out.println(num + "는 소수입니다.");}
+		else {System.out.println(num + "은 정수입니다.");}
+		
+		//100이하의 소수를 출력하는 예제
+		//num를 2부터 100까지 1씩 증가
+			//num가 소수이면 num를 출력
+		System.out.println("------------");
+		for(num = 2; num <= 100; ++num) {
+			count = 0;
+			for(int i = 1; i <= num; ++i) {
+				if(num % i == 0) {
+					count = count + 1;
+					}
+			}
+			if(count == 2) {
+				System.out.print(num + ", ");
+				}
+		}
+	}
+
+}
