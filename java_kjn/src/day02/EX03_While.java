6b7b579a7db8c07e79bc5e5858d26fee8a308dd6
netizen368 <rlawlsdud20302@gmail.com>
2025-12-02 16:35:17 +0900

    자바 2일차 : While문 예제

---
+++ java_kjn/src/day02/EX03_While.java
@@ -0,0 +1,46 @@
+package day02;
+
+public class EX03_While {
+
+	public static void main(String[] args) {
+
+		/* while문 문법
+		 * 
+		 * while(조건식){
+		 * 		실행문;
+		 * }
+		 * 
+		 * fot문과 비교를 위해 while문 추가. 실제 문법은 위를 참고
+		 * 초기화;
+		 * while(조건식){
+		 * 		실행문;
+		 * 		증감식;//실제 while문에서 증감식 위치는 없음. 그냥 실행문
+		 * }
+		 * 실행순서
+		 * - 조건식(거짓) => 반복문 종료
+		 * - 조건식(참) => 실행문
+		 * => 조건식(참) => 실행문
+		 * => 조건식(거짓) => 반복문 종료
+		 * 
+		 * while문에서 조건식은 생략할 수 없음
+		 */
+		
+		for(int i = 1; i <= 5; i++) {
+			System.out.print(i);
+		}
+		System.out.println();
+		//1부터 5까지 while문으로 출력
+		int i = 1;
+		while(i <= 5) {
+			System.out.print(i);
+			i++;
+		}
+		System.out.println();
+		//1부터 5까지 while문으로 출력
+		i = 0;
+		while(++i <= 5) {
+			System.out.print(i);
+		}
+	}
+
+}
