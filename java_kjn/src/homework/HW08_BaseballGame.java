493d24320a2f19e47acb538a8fa832eb4c619339
netizen368 <rlawlsdud20302@gmail.com>
2025-12-05 17:51:46 +0900

    숙제

--- java_kjn/src/homework/HW08_BaseballGame.java
+++ java_kjn/src/homework/HW08_BaseballGame.java
@@ -23,7 +23,34 @@ public class HW08_BaseballGame {
 		 * 입력 : 1 5 4
 		 * 정답입니다.
 		 */
-
+		int [] Baseball = new int[3];
+		int min = 1, max = 9;
+		
+		for(int i = 0; i < 3; ) {
+			int r = (int)(Math.random() * (max - min + 1) + min);
+			if(r != Baseball[0] && r != Baseball[1] && r != Baseball[2] ) {
+				Baseball[i] = r;
+				System.out.print(Baseball[i] + " ");
+				i++;
+			}
+		}
+		//반복문으로 같은 번지에 있는 걸 찾고 그걸 S, 이중 반복문으로 전체 번지에 숫자가 동일한게 있으면 B 갯수 찾기
+		//S 정의
+		//Baseball[0]이 p1와 동일하면 count2 + 1 
+		//Baseball[1]이 p2와 동일하면 count2 + 1 
+		//Baseball[2]이 p3와 동일하면 count2 + 1 
+		//if로 count2가 1개이면 S1, 2개이면 S2=> S
+		//B 정의
+		//Baseball[0]이 p1와 같이 않고 p2나 p3이랑 동일하면 count1 + 1 
+		//Baseball[1]이 p2와 같이 않고 p1나 p3이랑 동일하면 count1 + 1 
+		//Baseball[2]이 p3와 같이 않고 p1나 p2이랑 동일하면 count1 + 1 
+		//if로 count1가 1개이면 B1, 2개이면 B2, 3개이면 B3 => B
+		
+		
+		
+		//표시
+		//B, S가 있다면 표시, 없다면 O를 표시 후 입력창 활성화
+		//count2가 3개이면 정답입니다.를 표시하고 활동 종료
 	}
 
 }
