f7cbd05026b10dcad5e392daa6c6d69a7f53bd36
netizen368 <rlawlsdud20302@gmail.com>
2025-12-10 13:13:53 +0900

    수업

---
+++ java_kjn/src/day08/Client01.java
@@ -0,0 +1,63 @@
+package day08;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Client01 {
+
+	private Socket socket;
+	
+	private final static String EXIT = "EXIT";
+	
+	public void send() {
+		Thread t2 = new Thread(()->{
+			System.out.println("[전송기능 실행]");
+							
+			Scanner scan = new Scanner(System.in);
+			try {
+				ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+				//메시지를 여러번 전송하기 위한 무한 루프
+				while(true) {
+					//메시지 콘솔에 입력
+					System.out.print("입력 : ");
+					String msg = scan.nextLine();
+					// 서버로 전송
+					oos.writeUTF(msg);
+					oos.flush();
+					if(msg.equals(EXIT)) {
+						break;
+					}
+				}
+			} catch (IOException e) {
+				System.err.println("예외 발생");
+			}
+		});
+		t2.start();
+	}
+	public void receive() {
+		Thread t1 = new Thread(()->{
+			System.out.println("[수신기능 실행]");
+			try {
+				//클라이언트가 보낸 값을 받기 위한 입력 스트림객체 생성
+				ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+				//메시지를 여러번 입력할 수 있게 무한 루프
+				while(true) {
+					String msg = ois.readUTF();
+					System.out.println("메세지 : "+ msg);
+					if(msg.equals("EXIT")) {
+						System.out.println("전송을 종료했습니다.");
+						break;
+					}
+				}
+			} catch (IOException e) {
+				System.err.println("예외가 발생했습니다.");
+			}
+			System.out.println("[수신 스레드 종료]");
+		});
+		t1.start();
+	}
+}
