815a3b6d1924bb991627666d6eaab8b568c29e21
netizen368 <rlawlsdud20302@gmail.com>
2025-12-15 12:21:00 +0900

    12.15 수업내용

--- db/03_DML.sql
+++ db/03_DML.sql
@@ -105,7 +105,10 @@ values(4, 1, 50), (4, 2, 100);
 insert into score(st_code, sj_code, score)
 values(4, 1, 100), (4, 2, 50);
 
-
+# 1학년 1반에 새로운 학생이 전학을 왔다. 이때 필요한 쿼리를 작성
+# 이름은 가나다. 번호는 3번
+insert into sample.student(grade, class, num, name) 
+values(1,1,3,"가나다");
 
 
 
