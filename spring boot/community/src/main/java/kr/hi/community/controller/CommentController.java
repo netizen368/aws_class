b1657b0602d127962ebdbae56565b4aeb5c82c8d
netizen368 <rlawlsdud20302@gmail.com>
2026-01-12 12:11:09 +0900

    댓글 등록

--- spring boot/community/src/main/java/kr/hi/community/controller/CommentController.java
+++ spring boot/community/src/main/java/kr/hi/community/controller/CommentController.java
@@ -1,13 +1,34 @@
 package kr.hi.community.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.hi.community.model.dto.CommentDTO;
+import kr.hi.community.model.util.CustomUser;
 import kr.hi.community.serivce.commentService;
 
 @RestController //@ResponseBody + @Controller
+@RequestMapping("/commnet")
 public class CommentController {
 	
 	@Autowired
 	commentService commentService;
+	
+	@PostMapping("/insert")
+	public ResponseEntity<String> insert(
+		@RequestBody CommentDTO commentDto, 
+		@AuthenticationPrincipal CustomUser customUser){
+		// 서비스에게 댓글 정보와 사용자 정보를 주면서 댓글을 등록하고,
+		// 등록 결과를 문자열로 가져오도록 요청
+		//결과 = 서비스야.댓글등록해(댓글정보, 사용자정보);
+		String result = 
+			commentService.insertCommnet(commentDto, customUser);		
+		return ResponseEntity.ok(result);
+	}
 }
+	
--- spring boot/community/src/main/java/kr/hi/community/controller/PostController.java
+++ spring boot/community/src/main/java/kr/hi/community/controller/PostController.java
@@ -75,7 +75,8 @@ public class PostController {
 	
 	@GetMapping("/post/detail/{num}")
 	public String postDetail(
-		@PathVariable("num")int po_num, Model model) {
+		@PathVariable("num")int po_num, Model model,
+		@AuthenticationPrincipal CustomUser customUser) {
 		//게시글 번호를 이용해서 조회수 증가
 		postService.updateView(po_num);
 		
@@ -91,6 +92,8 @@ public class PostController {
 		model.addAttribute("post", post);
 		//가져온 첨부파일 목록을 화면에 전달
 		model.addAttribute("files", files);
+		model.addAttribute("user", customUser == null? "" : 
+			customUser.getUsername());
 		return "post/detail";
 	}
 	
