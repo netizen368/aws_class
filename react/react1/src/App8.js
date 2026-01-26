376fc86aba72be09de6331bd844f3bc7094178c8
netizen368 <rlawlsdud20302@gmail.com>
2026-01-26 17:21:25 +0900

    리액트 연습

---
+++ react/react1/src/App8.js
@@ -0,0 +1,30 @@
+
+function App8(){
+	//객체 배열
+	const list = [
+		{
+			name : "홍길동",
+			age : 21
+		},
+		{
+			name : "둘리",
+			age : 21
+		},
+		{
+			name : "고길동",
+			age : 41
+		}
+	]
+	return(
+		<div>
+			<ul>
+				<li>
+					<span>홍길동 : </span>
+					<span>21</span>
+				</li>
+			</ul>
+		</div>
+	);
+}
+
+export default App8;
