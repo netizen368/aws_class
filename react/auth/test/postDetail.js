aa25dc64de3ecbf141539109a6bfbfc9489f5d34
netizen368 <rlawlsdud20302@gmail.com>
2026-02-06 09:36:42 +0900

    e

--- react/auth/test/postDetail.js
+++ react/auth/test/postDetail.js
@@ -2,6 +2,29 @@ import { useEffect } from "react";
 
 export function PostDetail({ poNum, page }) {
 
+	const commentArea = document.getElementById("commentArea");
+
+	commentArea.innerHTML = "";
+
+	if (!data.list || data.list.length === 0) {
+			commentArea.innerHTML = "등록된 댓글이 없습니다.";
+	} else {
+
+			data.list.forEach(comment => {
+
+					const html = `
+							<div>
+									<p>작성자 : ${comment.writer}</p>
+									<p>내용 : ${comment.content}</p>
+									<p>작성일 : ${comment.regDate}</p>
+							</div>
+					`;
+
+					commentArea.innerHTML += html;
+			});
+
+	}
+
 	const loadComments = async () => {
 			try {
 					const response = await fetch(
